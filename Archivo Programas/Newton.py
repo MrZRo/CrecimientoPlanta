6444e467a41a84523a6ce874b7666a9ef45bc675
MrMagoo <107434436+MrZRo@users.noreply.github.com>
2025-04-16 22:59:52 -0400

    Update Newton.py

--- Archivo Programas/Newton.py
+++ Archivo Programas/Newton.py
@@ -1,20 +1,20 @@
 import numpy as np
 
-# Datos del crecimiento de la palmera cocotera (Días vs Altura en cm)
-x_days = np.array([7, 21, 35, 49, 63, 77, 91])  # Días
-y_height_cm = np.array([5, 12, 30, 46, 60, 74, 91])  # Altura en cm
+# Datos del crecimiento de la Palmera Cocotera (Días vs Altura en cm)
+x = np.array([7, 21, 35, 49, 63, 77, 91])  # Días
+y = np.array([5, 12, 30, 46, 60, 74, 91])  # Altura en cm
 # -------------------------
-# Datos del crecimiento de la palmera cocotera (Días vs Altura en cm)
-#x_days = np.array([7, 21, 35, 49, 63, 77, 91])  # Días
-#y_height_cm = np.array([5, 12, 30, 46, 60, 74, 91])  # Altura en cm
+# Datos del crecimiento del Arbol de la Princesa (Días vs Altura en cm)
+#x = np.array([30,60,90,120,150,180])  # Días
+#y = np.array([50,100,150,200,250,300])  # Altura en cm
 # -------------------------
-# Datos del crecimiento de la palmera cocotera (Días vs Altura en cm)
-#x_days = np.array([7, 21, 35, 49, 63, 77, 91])  # Días
-#y_height_cm = np.array([5, 12, 30, 46, 60, 74, 91])  # Altura en cm
+# Datos del crecimiento del tulipan (Días vs Altura en cm)
+#x = np.array([7, 21, 35, 49, 63, 77, 91])  # Días
+#y = np.array([5.2, 15.8, 32.5, 48.7, 58.2, 62.1, 62.5])  # Altura en cm
 # -------------------------
-# Datos del crecimiento de la palmera cocotera (Días vs Altura en cm)
-#x_days = np.array([7, 21, 35, 49, 63, 77, 91])  # Días
-#y_height_cm = np.array([5, 12, 30, 46, 60, 74, 91])  # Altura en cm
+# Datos del crecimiento del Diente de Leon (Días vs Altura en cm)
+#x = np.array([7, 21, 35, 49, 63, 77, 91])  # Días
+#y = np.array([2.5,6.8,12.3,18.7,25.0,30.2,35.2])  # Altura en cm
 
 # -------------------------
 # FUNCIONES DE NEWTON
